5b94036312d71016fedc8e61378983ea2528be36
Guilherme vinicios conde <guicondee@outlook.com>
2024-10-16 08:27:19 -0300

    conflict with path alias resolved

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		alias: {
-			'$components': '/src/components*'
-		},
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+  kit: {
+    alias: {
+      $components: "/src/components",
+    },
+    adapter: adapter(),
+  },
+  preprocess: vitePreprocess(),
 };
 export default config;
